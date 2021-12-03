ddd5c23b749416edae6982d11287acefb60e38ac
Gerome Marc Braddock <braddockgerome@gmail.com>
2021-12-03 09:53:02 +0000

    Update day_3_1.py

--- day_3/day_3_1.py
+++ day_3/day_3_1.py
@@ -1,4 +1,4 @@
 # Answer should be 4001724
 z=open("a").read().split("\n")
-x="".join(["1"if sum([int(bit) for bit in i])>len(z)/2 else"0"for i in zip(*z)])
+x="".join(["1"if sum([int(y)for y in i])>len(z)/2else"0"for i in zip(*z)])
 print(int(x,2)*int(''.join("01"[i<"1"]for i in x),2))
